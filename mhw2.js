c064f182919a7550903bc79ecb253ec3410fef3b
simoplat <150624084+simoplat@users.noreply.github.com>
2025-03-28 18:18:02 +0100

    update
    bottone menu fa scomparire la sidebar

--- mhw2.js
+++ mhw2.js
@@ -1,1 +1,13 @@
 let element = document.querySelector('#button-menu');
+const layout = document.querySelector('.central-layout');
+
+element.addEventListener('click', () => {
+    let sidebarContent = document.querySelector('.left-sidebar');
+    let currentDisplay = getComputedStyle(sidebarContent).display;
+
+    if (currentDisplay === 'none') {
+        sidebarContent.style.display = 'block';
+    } else {
+        sidebarContent.style.display = 'none';
+    }
+});
