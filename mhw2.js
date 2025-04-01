749f69c7f1ca4a36d0c3c89d4c7cc63847b40e2a
simoplat <150624084+simoplat@users.noreply.github.com>
2025-04-01 17:02:33 +0200

    Miglioramento funzione 1 e separaz. respons,

--- mhw2.js
+++ mhw2.js
@@ -4,18 +4,15 @@ const layout = document.querySelector('.central-layout');
 
 function toggleMenuSidebar(){
     let sidebarContent = document.querySelector('.left-sidebar');
-    let currentDisplay = getComputedStyle(sidebarContent).display;
-
-    if (currentDisplay === 'none') {
-        sidebarContent.style.display = 'block';
-        layout.style.width ='calc(100vw - 300px)'; 
-        layout.style.height ='calc(100vh - 60px)'; 
-        layout.style.left = '300px'; 
+    
+    if (sidebarContent.classList.contains('hidden')) {
+        sidebarContent.classList.remove('hidden');
+        layout.classList.remove('expand');
+        console.log('A'); //debug
     } else {
-        sidebarContent.style.display = 'none';
-        layout.style.left = '0px';
-        layout.style.width ='100%';
-        layout.style.height ='100%';
+        console.log('B'); //motivi di debug
+        sidebarContent.classList.add('hidden');
+        layout.classList.add('expand');
     }
 }
 
